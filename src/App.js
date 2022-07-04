b749d742f149726de607ef39472fe5b11eb70807
Shilpa <parasianshilpa@gmail.com>
2022-07-04 13:40:41 -0400

    todolist component

--- src/App.js
+++ src/App.js
@@ -6,14 +6,8 @@ import TodoList from './TodoList';
 
 function App() {
   return (
-    <>
     <TodoList /> 
-    <input type ="text" />
-    <button> Add Todo </button>
-    <button> Clear Completed </button>
-    <div> 0 left to do </div>
-    </>
-    );
+    )
 }
 
 export default App;
