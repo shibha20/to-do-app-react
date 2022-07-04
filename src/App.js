4a449ca482b0c0b71ab69d63676278554e647cdc
Shilpa <parasianshilpa@gmail.com>
2022-07-03 20:09:11 -0400

    build component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import TodoList from './TodoList';
+
 
 function App() {
   return (
-    null
+    <TodoList /> 
+
+
+
     );
 }
 
