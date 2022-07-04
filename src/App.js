7b96948f8dee9c0eec9a7dbe50fe6e23d85a30af
Shilpa <parasianshilpa@gmail.com>
2022-07-03 20:43:25 -0400

    webpage

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import TodoList from './TodoList';
 
 function App() {
   return (
+    <>
     <TodoList /> 
-
-
-
+    <input type ="text" />
+    <button> Add Todo </button>
+    <button> Clear Completed </button>
+    <div> 0 left to do </div>
+    </>
     );
 }
 
